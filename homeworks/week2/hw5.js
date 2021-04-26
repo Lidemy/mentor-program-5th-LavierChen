b63e357f42a6654ebd32d790598da6cb9543437b
Lavier Chen <lavier4869@gmail.com>
2021-04-26 20:23:26 +0800

    week2 revise

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,6 +1,6 @@
 function join(arr, concatStr) {
     if(arr.length === 0){
-        return ''            //考慮字串為空時的特別處理，若沒有額外處理，join('', '')會回傳 undefined
+        return ''            //考慮陣列為空時的特別處理，若沒有額外處理，join('', '') 會回傳 undefined
     }
     var result = arr[0]     //陣列的每個元素中間插入一個字串，因此最後一個元素後面不能插入 concatStr
     for(var i = 1; i < arr.length; i++) {
@@ -19,4 +19,4 @@ function repeat(str, times) {
 
 console.log(join(['a'], '!'));      //陣列裡只有一個元素，故不能插入 concatStr
 console.log(repeat('a', 5));
-console.log(join('', ''));          //空字串的特別處理
+console.log(join('', ''));          //空陣列的特別處理
